1244b601b693ec1ffe61af10e8debde04c0c9a97
babafemiolatona <femiolatona@gmail.com>
2023-06-08 22:46:11 +0200

    Update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 1):
+        print("0 arguments")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    elif len(sys.argv) > 2:
+        print(f"{len(sys.argv) - 1} arguments:")
+        for i in sys.argv[1:]:
+            print(f"{sys.argv.index(i)} : {i}")
