35b6abbc970d7ac4ed6322b75094ddbb3ebe9e80
babafemiolatona <femiolatona@gmail.com>
2023-06-08 22:49:50 +0200

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     if(len(sys.argv) == 1):
-        print("0 arguments")
+        print("0 arguments.")
     elif len(sys.argv) == 2:
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
