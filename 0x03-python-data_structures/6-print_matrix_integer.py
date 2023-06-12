ee4bf7e943daa3a07d8a6374610e2f7f15155f9d
babafemiolatona <femiolatona@gmail.com>
2023-06-12 20:43:36 +0200

    Update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        formatted_row = ""  
+        for j in i:
+            formatted_row += "{:d} ".format(j)        
+        print(formatted_row.strip())
