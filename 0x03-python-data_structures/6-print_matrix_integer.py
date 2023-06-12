779ced3a48c9545babe99b14c2965551bded26b5
babafemiolatona <femiolatona@gmail.com>
2023-06-12 20:45:33 +0200

    Removed whitespaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        formatted_row = ""  
+        formatted_row = ""
         for j in i:
-            formatted_row += "{:d} ".format(j)        
+            formatted_row += "{:d} ".format(j)
         print(formatted_row.strip())
