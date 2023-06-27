23de956b8229cd1f4f1d1bf157b8c1094346b3fb
babafemiolatona <femiolatona@gmail.com>
2023-06-27 23:59:07 +0200

    Refactored code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,15 +23,18 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
     def position(self):
         """Retrieves the position of the square"""
         return self.__position
 
+    @position.setter
     def position(self, value):
         """Sets the position of the square"""
         if not (isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(n, int) and n >= 0 for n in value)):
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -43,8 +46,9 @@ class Square:
         """Prints the square using the character "#" in stdout"""
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__position[1]):
-                print("#" * self.__size)
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
