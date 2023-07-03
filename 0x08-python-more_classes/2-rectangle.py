be448e7c3dade32da67d1dc3ca5ca42bea39c26b
babafemiolatona <femiolatona@gmail.com>
2023-07-03 16:06:57 +0200

    Update

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes a new instance of the Rectangle class"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates and returns the area of the rectangle"""
+        a = self.__width
+        b = self.__height
+        return a * b
+
+    def perimeter(self):
+        """Calculates and returns the perimeter of the rectangle"""
+        w = self.__width
+        b = self.__height
+        per = 2 * (w + b)
+        if w == 0 or b == 0:
+            per = 0
+        return "{}".format(per)
