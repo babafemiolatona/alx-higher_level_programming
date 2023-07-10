bf9c104cf2a08702f58f74173bd985e0f619ff6c
babafemiolatona <femiolatona@gmail.com>
2023-07-10 22:12:52 +0200

    Removed whitespaces

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,9 +14,8 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height
 
-
     def area(self):
         return self.__width * self.__height
-    
+
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
