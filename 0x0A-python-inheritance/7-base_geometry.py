c02346354aec5ae12ec06b5bef6530bcb52597b0
babafemiolatona <femiolatona@gmail.com>
2023-07-10 19:56:39 +0200

    Added files

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class BaseGeometry():
+    """Defines a class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.value = value
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
