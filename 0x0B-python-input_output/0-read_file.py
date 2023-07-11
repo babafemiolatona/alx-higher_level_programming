f72240691ebea0bd1f68813c040953d88a5eb7d4
babafemiolatona <femiolatona@gmail.com>
2023-07-11 16:25:04 +0200

    Initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
-        read_file = f.read()
-        print(read_file)
+        read_dt = f.read()
+        print(read_dt)
