8cb7f3114034ef75ce892e8ea6c87c573ee81ffd
babafemiolatona <femiolatona@gmail.com>
2023-07-11 17:46:40 +0200

    Update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and
+    returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        ret = f.write(text)
+    return ret
