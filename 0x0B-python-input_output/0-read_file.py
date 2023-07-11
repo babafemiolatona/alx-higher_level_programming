4d80d712d8b633e890d81137b3e17d8c8f5720ac
babafemiolatona <femiolatona@gmail.com>
2023-07-11 17:10:50 +0200

    Update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding="utf-8") as file:
-        for line in file:
-            print(line, end='')
-        print()
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end='')
