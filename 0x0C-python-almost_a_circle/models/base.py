e1e5bdd8b2402dc7b0d3f623176e41ad1d350b82
babafemiolatona <femiolatona@gmail.com>
2023-07-17 21:55:56 +0200

    Updated base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
