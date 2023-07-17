7251d66766efc6252ddb2128b5e449f12e89db6d
babafemiolatona <femiolatona@gmail.com>
2023-07-17 22:12:54 +0200

    Updated base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,16 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+
+        class_name = cls.__name__
+        filename = f"{class_name}.json"
+
+        with open(filename, "w") as file:
+            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            file.write(json_string)
