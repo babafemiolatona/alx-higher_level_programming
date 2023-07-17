0c9a73cac741616a0f5f669cefb8cf0200992710
babafemiolatona <femiolatona@gmail.com>
2023-07-17 21:31:25 +0200

    Update

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/pyhton3
+""" Square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square class that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square instance"""
+        self.__width = size
+        self.__height = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Retrieves the size of the Square instance"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the Square instance"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
