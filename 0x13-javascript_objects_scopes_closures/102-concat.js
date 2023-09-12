dea0852cdd0b977ace5bc443c88fde9f9b40809c
babafemiolatona <femiolatona@gmail.com>
2023-09-12 23:32:04 +0100

    Fixed errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,5 +3,5 @@
 const args = process.argv;
 args[2] = 'C is fun!';
 args[3] = '\nPython is Cool!!!';
-args[4] = args[2] + args[3];
-console.log(args[4]);
+// args[4] = args[2] + args[3];
+console.log(`${args[2]} ${args[3]}`);
