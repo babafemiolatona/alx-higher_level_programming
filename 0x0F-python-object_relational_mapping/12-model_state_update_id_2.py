207eea5535172913a93879e50e42fceff280c05c
babafemiolatona <femiolatona@gmail.com>
2023-09-17 23:45:56 +0100

    Refactored code

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
+"""
+Module that connects python script to a database
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(sys.argv[1],
-                                       sys.argv[2],
-                                       sys.argv[3]))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        state = session.query(State).filter(State.id == 2).first()
-        if state:
-            state.name = 'New Mexico'
-        session.commit()
-        session.close()
-    else:
-        print("Usage: ./12-model_state_update_id_2.py <username> <password> \
-            <database name> <new state name>")
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
+
+    my_session_maker = sessionmaker(bind=engine)
+    my_session = my_session_maker()
+
+    state = my_session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    my_session.commit()
+
+    my_session.close()
